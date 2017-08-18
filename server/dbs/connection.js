0097f39c62bf7060b09218d9924633572c7b9124
Ubuntu <835943102@qq.com>
2017-08-18 15:37:37 +0800

    insert

--- server/dbs/connection.js
+++ server/dbs/connection.js
@@ -1,22 +1,22 @@
-//noinspection JSUnresolvedFunction
 /**
  * Created by lmy on 17-8-18.
  */
-const mysql=require('mysql');
-//noinspection JSUnresolvedFunction
-var connection=mysql.createConnection(
+
+const mysql = require('mysql');
+
+var connection = mysql.createConnection(
     {
-        host:'localhost',
-        user:'root',
-        password:"lmy123321",
-        database:'dailySummary',
-        port:3306
+        host: 'localhost',
+        user: 'root',
+        password: "lmy123321",
+        database: 'dailySummary',
+        port: 3306
     });
-connection.connect((err)=>{
-    if(err){
-        console.log('err'+err.stack);
+connection.connect((err)=> {
+    if (err) {
+        console.log('err' + err.stack);
         return;
     }
     console.log('connected');
 });
-module.exports=connection;
+module.exports = connection;
--- server/dbs/dbMehods/titleInsert.js
+++ server/dbs/dbMehods/titleInsert.js
@@ -1,3 +1,24 @@
 /**
  * Created by lmy on 17-8-18.
  */
+'use strict'
+
+let express = require('express');
+let db = require('../connection');
+
+var  addSql = 'INSERT INTO edit(id,title,text) VALUES(0,?,?)';
+var  addSqlParams = ['test', 'mytext'];
+//增
+db.query(addSql,addSqlParams,function (err, result) {
+    if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+    }
+
+    console.log('--------------------------INSERT----------------------------');
+
+    console.log('INSERT ID:',result);
+    console.log('-----------------------------------------------------------------\n\n');
+});
+
+db.end();
